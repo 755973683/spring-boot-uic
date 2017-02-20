e5ef1ac4c93deda2dfe165fb20ee405ff9648416
755973683 <755973683@qq.com>
2017-02-20 10:54:16 +0800

    create step-01

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # spring-boot-uic
+    项目目的:熟悉采用Spring Boot构建简单应用项目
+
+#Setp-01
+  导入 start.spring.io 上创建的Boot项目
+  dependency:
+    1.spring-boot-starter-actuator
+    2.spring-boot-starter-thymeleaf
+    3.spring-boot-starter-web
+    4.spring-boot-devtools
+    5.lombok
+    6.spring-boot-starter-test
---
+++ src/main/java/com/qishon/QishonSpringBootUicApplication.java
@@ -0,0 +1,12 @@
+package com.qishon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QishonSpringBootUicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QishonSpringBootUicApplication.class, args);
+	}
+}
